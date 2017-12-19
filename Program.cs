1437425dec799555ce86c9ec7ded099b06594988
JesusLinares01 <34040095+JesusLinares01@users.noreply.github.com>
2017-12-19 14:34:08 -0600

    Sum of arrays

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Solution
+{
+    private static void Main(string[] args)
+    {
+        var ar_temp = Console.ReadLine().Split(' ');
+        var ar = Array.ConvertAll(ar_temp, int.Parse);
+        var result = 0;
+
+        for (var i = 0; i < ar.Length; i++)
+            result = result + ar[i];
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
